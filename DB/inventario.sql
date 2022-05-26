fa8b8a53e612eca162f88f46db5c549f3c90cb67
Cristian <palaciomorelos12@gmail.com>
2022-05-25 20:51:01 -0500

    4.0 (se actualizo la base de datos)

--- DB/inventario.sql
+++ DB/inventario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-04-2022 a las 21:53:22
+-- Tiempo de generación: 26-05-2022 a las 03:45:38
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -39,7 +39,33 @@ CREATE TABLE `categoria` (
 
 INSERT INTO `categoria` (`categoria_id`, `categoria_nombre`, `categoria_ubicacion`) VALUES
 (1, 'bebidas', ''),
-(2, 'no pedecedero', '');
+(2, 'no pedecedero', ''),
+(3, 'fruta', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `cliente_id` int(10) NOT NULL,
+  `cliente_td` varchar(10) NOT NULL,
+  `cliente_documento` varchar(30) NOT NULL,
+  `cliente_nombre` varchar(50) NOT NULL,
+  `cliente_apellido` varchar(50) NOT NULL,
+  `cliente_telefono` varchar(10) NOT NULL,
+  `cliente_email` varchar(50) NOT NULL,
+  `cliente_direccion` varchar(70) NOT NULL,
+  `cliente_ciudad` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`cliente_id`, `cliente_td`, `cliente_documento`, `cliente_nombre`, `cliente_apellido`, `cliente_telefono`, `cliente_email`, `cliente_direccion`, `cliente_ciudad`) VALUES
+(3, 'CC', '1007973563', 'Cristian David', 'Palacio Morelos', '3105437199', 'cpalaciom@unicartagena.edu.co', 'zaragocilla calle 27 #53-55', 'Cartagena');
 
 -- --------------------------------------------------------
 
@@ -64,8 +90,8 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`producto_id`, `producto_codigo`, `producto_nombre`, `producto_peso`, `producto_precio`, `producto_stock`, `producto_foto`, `categoria_id`, `usuario_id`) VALUES
-(4, '474574574', 'Arroz diana', '5.40', '1800.00', 5, '', 1, 1),
-(6, '8686679', 'sal refisal', '250.00', '3200.00', 43, '', 2, 1);
+(23, '554546', 'Arroz diana', '500.00', '2000.00', 15, 'Arroz_diana_66.jpg', 2, 1),
+(24, '65756436', 'Leche colanta', '900.00', '3700.00', 0, 'Leche_colanta_30.jpg', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -75,11 +101,20 @@ INSERT INTO `producto` (`producto_id`, `producto_codigo`, `producto_nombre`, `pr
 
 CREATE TABLE `producto_vendido` (
   `pv_id` int(11) NOT NULL,
-  `venta_id` int(11) NOT NULL,
+  `venta_codigo` varchar(10) NOT NULL,
   `producto_id` int(20) NOT NULL,
   `pv_stock` int(25) NOT NULL,
-  `pv_total` int(25) NOT NULL
+  `pv_total` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producto_vendido`
+--
+
+INSERT INTO `producto_vendido` (`pv_id`, `venta_codigo`, `producto_id`, `pv_stock`, `pv_total`) VALUES
+(100, '1000000001', 23, 8, 16000),
+(101, '1000000002', 23, 5, 10000),
+(102, '1000000002', 24, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -93,16 +128,17 @@ CREATE TABLE `usuario` (
   `usuario_apellido` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
   `usuario_usuario` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
   `usuario_clave` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
-  `usuario_email` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL
+  `usuario_email` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `rol` varchar(5) COLLATE utf8_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`usuario_id`, `usuario_nombre`, `usuario_apellido`, `usuario_usuario`, `usuario_clave`, `usuario_email`) VALUES
-(1, 'Administrador', 'Principal', 'Administrador', '$2y$10$EPY9LSLOFLDDBriuJICmFOqmZdnDXxLJG8YFbog5LcExp77DBQvgC', ''),
-(2, 'Cristian', 'palacio', 'Cristian', 'Cristian123', 'palaciomorelos12@gmail.com');
+INSERT INTO `usuario` (`usuario_id`, `usuario_nombre`, `usuario_apellido`, `usuario_usuario`, `usuario_clave`, `usuario_email`, `rol`) VALUES
+(1, 'Administrador', 'Principal', 'Administrador', '$2y$10$EPY9LSLOFLDDBriuJICmFOqmZdnDXxLJG8YFbog5LcExp77DBQvgC', '', 'admin'),
+(2, 'Cristian', 'palacio', 'Cristian', 'Cristian123', 'palaciomorelos12@gmail.com', '');
 
 -- --------------------------------------------------------
 
@@ -112,8 +148,9 @@ INSERT INTO `usuario` (`usuario_id`, `usuario_nombre`, `usuario_apellido`, `usua
 
 CREATE TABLE `venta` (
   `venta_id` int(11) NOT NULL,
-  `venta_codigo` varchar(70) NOT NULL,
-  `venta_fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `venta_codigo` varchar(10) NOT NULL DEFAULT '0',
+  `venta_fecha` date DEFAULT NULL,
+  `cliente_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -125,6 +162,12 @@ CREATE TABLE `venta` (
 --
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`categoria_id`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cliente_id`);
 
 --
 -- Indices de la tabla `producto`
@@ -139,7 +182,7 @@ ALTER TABLE `producto`
 --
 ALTER TABLE `producto_vendido`
   ADD PRIMARY KEY (`pv_id`),
-  ADD KEY `venta_id` (`venta_id`,`producto_id`),
+  ADD KEY `venta_id` (`venta_codigo`),
   ADD KEY `producto_id` (`producto_id`);
 
 --
@@ -152,7 +195,9 @@ ALTER TABLE `usuario`
 -- Indices de la tabla `venta`
 --
 ALTER TABLE `venta`
-  ADD PRIMARY KEY (`venta_id`);
+  ADD PRIMARY KEY (`venta_id`),
+  ADD KEY `venta_codigo` (`venta_codigo`),
+  ADD KEY `cliente_id` (`cliente_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -162,19 +207,25 @@ ALTER TABLE `venta`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `categoria_id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `categoria_id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `cliente_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `producto_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `producto_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `producto_vendido`
 --
 ALTER TABLE `producto_vendido`
-  MODIFY `pv_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `pv_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -186,7 +237,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `venta`
 --
 ALTER TABLE `venta`
-  MODIFY `venta_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `venta_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -196,15 +247,21 @@ ALTER TABLE `venta`
 -- Filtros para la tabla `producto`
 --
 ALTER TABLE `producto`
-  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`categoria_id`),
-  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`);
+  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`),
+  ADD CONSTRAINT `producto_ibfk_3` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`categoria_id`);
 
 --
 -- Filtros para la tabla `producto_vendido`
 --
 ALTER TABLE `producto_vendido`
-  ADD CONSTRAINT `producto_vendido_ibfk_1` FOREIGN KEY (`venta_id`) REFERENCES `venta` (`venta_id`),
-  ADD CONSTRAINT `producto_vendido_ibfk_2` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`producto_id`);
+  ADD CONSTRAINT `producto_vendido_ibfk_3` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`producto_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`venta_codigo`) REFERENCES `producto_vendido` (`venta_codigo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`cliente_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
